3a4c5c83a60575bb0472c178e40f604576570a59
Matthew Cohen <cohen.matthew@ufl.edu>
2023-01-10 15:49:20 -0500

    added loop to allocate fakes as well

--- ggsolver/decoy_alloc/solvers.py
+++ ggsolver/decoy_alloc/solvers.py
@@ -35,10 +35,14 @@ def greedy_max(graph, trap_subsets, fake_subsets, max_traps=float("inf"), max_fa
             states.add(state)
 
     arena_traps = set() # set of arena points
+    arena_fakes = set() # set of arena points
+
     covered_states = set() # set of states
     trap_states = set() # set of states
+    fake_states = set() # set of states
+
     iter_count = 0
-    # TODO repeat this loop again to allocate fake targets (the first pass allocates traps)
+    # Allocate traps
     while len(states - covered_states) > 0 and len(arena_traps) < max_traps:
         iter_count += 1
         print(f"Iteration {iter_count}")
@@ -65,6 +69,36 @@ def greedy_max(graph, trap_subsets, fake_subsets, max_traps=float("inf"), max_fa
 
         print(f"\tSelected Trap: {next_trap['arena_point']}")
         print(f"\tNew total trap states: {len(trap_states)}")
+        print(f"\tNew total winning states: {len(covered_states)}")
+
+    # Allocate fakes
+    while len(states - covered_states) > 0 and len(arena_fakes) < max_fakes:
+        iter_count += 1
+        print(f"Iteration {iter_count}")
+
+        # Potential points for fakes are all points that are not fakes OR TRAPS
+        non_allocated_points = arena_points - arena_traps - arena_fakes
+        updated_winning_regions = list()
+        print(f"\tnon_allocated_points: {non_allocated_points}")
+
+        for arena_point in non_allocated_points:
+            # the list of final states if this arena point is made into a fake (we must include states made winning by traps)
+            final_states = list(fake_states) + list(trap_states) + fake_subsets[arena_point]
+
+            solver = SWinReach(graph, final=final_states)
+            solver.solve()
+            pair = { "arena_point": arena_point, "winning_states": solver.win_region(1) }
+
+            updated_winning_regions.append(pair)
+
+        # TODO what to do if two traps/fakes give the same number of winning states? does it matter which we pick?
+        next_fake = max(updated_winning_regions, key=lambda x: len(x["winning_states"]))
+        arena_fakes.add(next_fake["arena_point"])
+        fake_states.update(fake_subsets[next_fake["arena_point"]])
+        covered_states.update(next_fake["winning_states"])
+
+        print(f"\tSelected Fake: {next_fake['arena_point']}")
+        print(f"\tNew total fake states: {len(fake_states)}")
         print(f"\tNew total winning states: {len(covered_states)}")
 
     return arena_traps, covered_states

6c29dd26c9f91b96395d6fec96ecaa5071fa2688
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2023-03-24 14:41:49 -0400

    #add: Terrain class

--- examples/apps/tom_and_jerry/main.py
+++ examples/apps/tom_and_jerry/main.py
@@ -1,7 +1,8 @@
 """
 Programmer's notes:
-
-
+    - Input grid coordinate is XY.
+    - TomJerryGame terrain is XY.
+    - Only in rendering we will use Row-Column to represent gridworld.
 """
 import json
 
@@ -108,6 +109,7 @@ class TomJerryWindow(gw.Window):
             visible=True,
             init_sprite="front",
         )
+
     def add_fake(self, x, y):
         self._fake = Character(
             name="fake",
@@ -120,6 +122,7 @@ class TomJerryWindow(gw.Window):
             visible=True,
             init_sprite="front",
         )
+
     def sm_update(self, sender, event_args):
         print(f"sm_update: {event_args}")
 
@@ -182,13 +185,14 @@ class Character(gw.Control):
 
     def _on_select_changed(self, sender, event_args):
         self._is_selected = not self._is_selected
-    
+
     def update(self):
         super(Character, self).update()
 
         if self._curr_sprite is not None:
             if self._sprites[self._curr_sprite] is None:
-                self._sprites[self._curr_sprite] = pygame.image.load(os.path.join(curr_file_path, pathlib.Path(self._sprite_files[self._curr_sprite])))
+                self._sprites[self._curr_sprite] = pygame.image.load(
+                    os.path.join(curr_file_path, pathlib.Path(self._sprite_files[self._curr_sprite])))
                 self._sprites[self._curr_sprite] = pygame.transform.scale(self._sprites[self._curr_sprite], (50, 50))
             self._backimage = self._sprites[self._curr_sprite]
 
@@ -222,25 +226,55 @@ class Character(gw.Control):
             self.visible = not self.visible
 
 
+class Terrain:
+    def __init__(self, terrain_array: np.ndarray):
+        self._terrain = terrain_array
+        self._y_max, self._x_max = self._terrain.shape
+
+    def __contains__(self, item):
+        x, y = item
+        return 0 <= x < self.x_max and 0 <= y < self.y_max
+
+    def __getitem__(self, item):
+        assert self.__contains__(item)
+        x, y = item
+        return self._terrain[self._y_max - y - 1, x]
+
+    def __repr__(self):
+        return np.array2string(self._terrain)
+
+    @property
+    def shape(self):
+        return self._x_max, self._y_max
+
+    @property
+    def x_max(self):
+        return self._x_max
+    
+    @property
+    def y_max(self):
+        return self._y_max
+
+
 class TomJerryGame(dtptb.DTPTBGame):
     def __init__(self, game_config):
         super(TomJerryGame, self).__init__()
         with open(game_config, "r") as file:
             self._game_config = json.load(file)
 
-        self._terrain = self._orient_terrain(np.array(self._game_config["terrain"]))
-        self._grid_size = (tuple(self._terrain.shape))
-        self._x_max = self._grid_size[0] - 1
-        self._y_max = self._grid_size[1] - 1
+        self._terrain = Terrain(np.array(self._game_config["terrain"]))
         self._cheese_location = self._game_config["cheese"]["cheese.1"]
 
         # TODO what should the order of x and y be here?
-        self._walkable_cells = [(x, y) for x in range(self._x_max) for y in range(self._y_max) if self._terrain[self.grid_coordinates_to_terrain(x, y)] == 1]
-        self._door_locations = [(x, y) for x in range(self._x_max) for y in range(self._y_max) if self._terrain[self.grid_coordinates_to_terrain(x, y)] == 2]
+        self._walkable_cells = [(x, y) for x in range(self._terrain.x_max) for y in range(self._terrain.y_max) if
+                                self._terrain[x, y] == 1]
+        self._door_locations = [(x, y) for x in range(self._terrain.x_max) for y in range(self._terrain.y_max) if
+                                self._terrain[x, y] == 2]
         print(self._walkable_cells)
         self._states = self._construct_states()
         self._final = self._construct_final()
         self._tom_winning_states = self._construct_tom_winning_states()
+
     def _construct_states(self):
         """
         State representation: (tom.cell, jerry.cell, door_states, turn)
@@ -248,10 +282,11 @@ class TomJerryGame(dtptb.DTPTBGame):
         """
         unique, counts = np.unique(self._terrain, return_counts=True)
         number_of_doors = counts[2]
-        possible_door_states = list(itertools.product([0,1], repeat=number_of_doors))
+        possible_door_states = list(itertools.product([0, 1], repeat=number_of_doors))
         return list(
             filter(self._is_state_valid,
-                   itertools.product(self._walkable_cells, self._walkable_cells, possible_door_states, [CheeseState.TOM_TURN, CheeseState.JERRY_TURN]))
+                   itertools.product(self._walkable_cells, self._walkable_cells, possible_door_states,
+                                     [CheeseState.TOM_TURN, CheeseState.JERRY_TURN]))
         )
 
     def _move(self, cell, act, steps=1):
@@ -271,6 +306,7 @@ class TomJerryGame(dtptb.DTPTBGame):
 
         elif act == gw_utils.GW_ACT_STAY:
             return cell
+
     def states(self):
         return self._states
 
@@ -328,6 +364,7 @@ class TomJerryGame(dtptb.DTPTBGame):
             return True
         else:
             return False
+
     def final(self, state):
         return state in self._final
 
@@ -337,16 +374,19 @@ class TomJerryGame(dtptb.DTPTBGame):
     def turn(self, state):
         tom_cell, jerry_cell, door_states, turn = state
         return turn
+
     def _is_state_valid(self, state):
         tom_cell, jerry_cell, door_states, turn = state
 
         # if tom or jerry are in a wall
-        if self._terrain[self.grid_coordinates_to_terrain(tom_cell[0], tom_cell[1])] == 0 or self._terrain[self.grid_coordinates_to_terrain(jerry_cell[0], jerry_cell[1])] == 0:
+        if self._terrain[self.grid_coordinates_to_terrain(tom_cell[0], tom_cell[1])] == 0 or self._terrain[
+            self.grid_coordinates_to_terrain(jerry_cell[0], jerry_cell[1])] == 0:
             return False
 
         # if jerry or tom is in a CLOSED door
         for index, door in enumerate(self._door_locations):
-            if jerry_cell == door and door_states[index] == CheeseState.DOOR_CLOSED or tom_cell == door and door_states[index] == CheeseState.DOOR_CLOSED:
+            if jerry_cell == door and door_states[index] == CheeseState.DOOR_CLOSED or tom_cell == door and door_states[
+                index] == CheeseState.DOOR_CLOSED:
                 return False
         return True
 
@@ -355,12 +395,14 @@ class TomJerryGame(dtptb.DTPTBGame):
 
     def _construct_tom_winning_states(self):
         return list(filter(self._tom_wins_state, self.states()))
+
     def _tom_wins_state(self, state):
         tom_cell, jerry_cell, door_states, turn = state
         if tom_cell == jerry_cell:
             return True
         else:
             return False
+
     def _matrix_flip(self, mat, axis):
         if not hasattr(mat, 'ndim'):
             mat = np.asarray(mat)
@@ -375,22 +417,26 @@ class TomJerryGame(dtptb.DTPTBGame):
     def grid_coordinates_to_terrain(self, x, y):
         # This function is needed because self._terrain is flipped in both axes
         return self._x_max - x, self._y_max - y
+
     def _orient_terrain(self, mat):
         return self._matrix_flip(np.transpose(mat), axis=0)
+
 
 class CheeseState:
     TOM_TURN = 1
     JERRY_TURN = 2
     DOOR_CLOSED = 0
     DOOR_OPEN = 1
+
     def __init__(self, tom_cell, jerry_cell, door_array, turn):
         self.tom_cell = tom_cell
         self.jerry_cell = jerry_cell
         self.door_array = door_array
         self.turn = turn
 
+
 if __name__ == '__main__':
-    conf = os.path.join(curr_file_path, "saved_games", "game_2023_02_17_17_36.conf")
+    conf = os.path.join(curr_file_path, "saved_games", "game_2023_03_24_18_13.conf")
     print(f"Using configuration file: {conf=}")
 
     ## Create Base Reachability Game ##
@@ -415,7 +461,8 @@ if __name__ == '__main__':
     base_solver.solve()
     # TODO create this subgraph
     tom_jerry_subgraph = gg_graph.SubGraph(base_graph)
-    tom_jerry_subgraph.hide_nodes(base_solver.winning_nodes(1)) # TODO I think the winning_nodes method is missing from this branch
+    tom_jerry_subgraph.hide_nodes(
+        base_solver.winning_nodes(1))  # TODO I think the winning_nodes method is missing from this branch
     # TODO ask AK
     #  Should there be any non-rationalizable edges left? Those would just be edges that go from win1 to win2 in the
     #  original game right? Since we removed win1 from this game and are only looking at win2 are all actions rationalizable?
@@ -436,12 +483,12 @@ if __name__ == '__main__':
     p2_perceptual_game = decoy_models.PerceptualGameP2(game=tom_jerry_game, traps=trap_states, fakes=fake_states)
     ## Solve p2's perceptual game ##
     solution_p2_perceptual_game = reach.SWinReach(tom_jerry_subgraph, final=tom_jerry_game.final_states())
-    solution_p2_perceptual_game.solve() # P2's strategy
+    solution_p2_perceptual_game.solve()  # P2's strategy
     ## Create Reachability Game of P1 ##
     p1_reachability_game = decoy_models.ReachabilityGameOfP1(
         p2_game=p2_perceptual_game, traps=trap_states, solution_p2_game=solution_p2_perceptual_game)
     ## Create Hypergame ##
-    hypergame = decoy_models.Hypergame( # P1's strategy
+    hypergame = decoy_models.Hypergame(  # P1's strategy
         p2_game=p2_perceptual_game, solution_p2_game=solution_p2_perceptual_game, traps=trap_states, fakes=fake_states)
 
     window = TomJerryWindow(name="Tom and Jerry", size=(660, 480), game_config=conf)

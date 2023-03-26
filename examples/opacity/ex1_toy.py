cb9cc6734f84d39bb09f9db872b3b839c15ed606
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2023-03-26 10:05:05 -0400

    #update: copied from chongyang

--- examples/opacity/ex1_toy.py
+++ examples/opacity/ex1_toy.py
@@ -1,40 +1,64 @@
 """
 Implement toy problem.
-
 Refer to dtptb/examples folder.
 """
 
-import examples.opacity.models as mod_opacity
-import ggsolver.graph as ggraph
+import models as mod_opacity
+import ggsolver.logic as logic
+# import ggsolver.graph as ggraph
+import ggsolver.dtptb as dtptb
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 
-class MyGame(mod_opacity.ReachabilityGame):
+class MyGame(mod_opacity.Arena):
     """
     # TODO. Implement this for your example.
     """
     def states(self):
-        pass
+        return list(range(7))
 
     def turn(self, state):
-        pass
+        if state in [0, 2, 6]:
+            return 1
+        else:
+            return 2
 
     def actions(self):
-        pass
+        return [(0,1),(0,4),(0,5),(1,2),(1,6),(2,3),(4,6),(5,2),(5,6),(6,3),(3,3)]
 
     def delta(self, state, act):
-        pass
+        if state == act[0]:
+            return act[1]
+        return None
 
     def atoms(self):
-        pass
+        return ["h1", "h2", "p3"]
 
     def label(self, state):
-        pass
+        if state == 5:
+            return ["h1"]
+        elif state == 5:
+            return ["h2"]
+        elif state == 3:
+            return ["p3"]
+        else:
+            return []
 
     def formula(self):
-        pass
+        return logic.ltl.ScLTL("Fh1 || Fh2 && Fp3")
 
-    def attacker_observation(self, state, act):
-        pass
+    def attacker_observation(self, state, act, next_state):
+        if (state in [1,4]) and (next_state in [1,4]):
+            return "1a4" + "1a4"
+        elif (state in [2,5,6]) and (next_state in [1,4]):
+            return "2ah" + "1a4"
+        elif (state in [1,4]) and (next_state in [2,5,6]):
+            return "1a4" + "2ah"
+        elif (state in [2,5,6]) and (next_state in [2,5,6]):
+            return "2ah" + "2ah"
+        else:
+            return str(state) + str(next_state)
 
 
 def solve(game: mod_opacity.BeliefGame):
@@ -43,32 +67,31 @@ def solve(game: mod_opacity.BeliefGame):
     :return:
     """
     # Graphify the game
-    graph = None
-
-    # Define a safety solver. (see dtptb.solvers.SWinSafe)
-    swin_safety = None
-
-    # Solve the safety game.
-    swin_safety.solve()
-
-    # Construct new graph using solution of safety game
-    solution = swin_safety.solution()
-    hidden_nodes = []       # Use swin_safety.node_winner function
-    hidden_edges = []       # Use swin_safety.winning_edges function
-    safe_graph = ggraph.SubGraph(graph=solution, hidden_nodes=hidden_nodes, hidden_edges=hidden_edges)
+    graph = game.graphify(pointed=True)
+    print("grphify done.")
 
     # Define a reachability solver (see dtptb.solvers.SWinReach)
-    swin_reach = None
+    swin_reach = dtptb.SWinReach(graph)
+    print("Swin_reach created")
 
     # Solve the safety game.
     swin_reach.solve()
+    print("SWIN rECH solved..")
+
+    print(f"{swin_reach.win_region(1)=}")
 
     # Return solution to reachability game.
     return swin_reach
 
 
-def main():
+if __name__ == "__main__":
     # Instantiate MyGame here
-    # Call solve()
-    # Analyze the output (see API for models.Solver)
-    pass
+    game = MyGame()
+    game.initialize(0)
+    aut = game.formula().translate()
+
+    belief_game = mod_opacity.BeliefGame(game, aut)
+    belief_game.initialize(belief_game.init_state())
+    solve(belief_game)
+
+    # Analyze the output (see API for models.Solver)#
--- examples/opacity/models.py
+++ examples/opacity/models.py
@@ -1,39 +1,68 @@
 """
 Models implementing paper on Opacity, CDC'23.
 """
+import itertools
 
+import ggsolver.util as util
 import ggsolver.dtptb as dtptb
+import ggsolver.logic as logic
+import ggsolver.models as models
 
 
-class ReachabilityGame(dtptb.DTPTBGame):
+class Arena(dtptb.DTPTBGame):
     """
     Defines input game. See Def. 1 + Def. 6 in paper.
-
     Def. 1 <=> dtptb.DTPTBGame
     attacker_observation <=> Def. 6.
     """
     EDGE_PROPERTY = dtptb.DTPTBGame.EDGE_PROPERTY.copy()
 
-    def attacker_observation(self, state, act):
+    @models.register_property(EDGE_PROPERTY)
+    def attacker_observation(self, state, act, next_state):
         raise NotImplementedError("Marked Abstract")
 
 
 class BeliefGame(dtptb.DTPTBGame):
-    def __init__(self, game: ReachabilityGame):
+    def __init__(self, game: Arena, aut: logic.automata.DFA):
         super(BeliefGame, self).__init__()
         self._game = game
+        self._aut = aut
 
-    def states(self):
-        pass
+    # def states(self):
+    #     T = itertools.product(self._game.states(), self._aut.states())
+    #     print(f"constructing powerset, {len(list(T))}")
+    #     PT = util.powerset(T)
+    #     # print(len(PT))
+    #     print(len(list(itertools.product(T,PT))))
+    #     return list(itertools.product(T,PT))
 
     def turn(self, state):
-        pass
+        return self._game.turn(state[0])
 
     def actions(self):
-        pass
+        return self._game.actions()
 
     def delta(self, state, act):
-        pass
+        s, q, b = state
+        t = self._game.delta(s, act)
+        p = self._aut.delta(q, self._game.label(t))
+        c = list()
+        for s_b, q_b in b:
+            for a_b in self._game.actions():
+                t_b = self._game.delta(s_b, a_b)
+                p_b = self._aut.delta(q_b, self._game.label(t_b))
+                if self._game.attacker_observation(s, act, t) == self._game.attacker_observation(s_b, a_b, t_b):
+                    c.append((t_b, p_b))
+        return t, p, tuple(c)
 
     def final(self, state):
-        pass
+        s, q, b = state
+        IfFinal = 0 in self._aut.final(q)
+        for s_b,q_b in b:
+            IfFinal = IfFinal & (0 in self._aut.final(q_b))
+        return IfFinal
+
+    def init_state(self):
+        s0 = self._game.init_state()
+        q0 = self._aut.init_state()
+        return s0, q0, ((s0, q0),)

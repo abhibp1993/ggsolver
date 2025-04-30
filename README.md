ca173d24266b7cd2d41bbae29a40926ed3363b05
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2025-04-30 13:49:19 -0500

    #bugfix: automatica2025 sim uses XY-coordinate system.
    Previously, the rendering function used row-column format whereas user input was in XY-coordinate. This discrepancy resulted in inconsistent rendering. This commit fixes the inconsistency.
    
    Also, rendering of semi-automaton & preference graph is improved.

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# ggsolver
-Python package to model, manipulate, and solve  games and hypergames on graphs. 
+`ggsolver` is a Python package to model, manipulate, and solve 
+games and hypergames on graphs. 
+
+[Note: This package is still in development and not all features are implemented yet. 
+The available algorithms are marked with check-marks below.]
+
+
+## Unique Features
+
+There exist several tools to model and solve games on graphs (e.g., PRISM, stormpy). 
+Why do we need `ggsolver`?
+
+1. **Hypergame support:** 
+    A hypergame is game of (potentially evolving) perceptual games of each player. 
+    `ggsolver` optimizes the modeling of hypergames on graphs by efficiently managing the perceptual games.
+2. **Integration with ML-frameworks:** 
+    `ggsolver` is designed to be integrated with ML-frameworks (e.g., PyTorch, TensorFlow) to enable the use of 
+    learning techniques for solving (hyper)games on graphs.
+3. **Incompatible solvers:** 
+    It is either tedious or not straightforward to model non-trivial transition models into PRISM, 
+    where next state is determined by complex if-else clauses.     
+
+
+## Algorithms 
+The package implements / interfaces several known algorithms for solving 
+games on graphs, including:
+* Sure/almost-sure winning in deterministic two-player turn-based games 
+  (Zielonka's strategy construction algorithm)
+* Sure/almost-sure winning in stochastic two-player turn-based games 
+* Sure winning in stochastic two-player concurrent games 
+* Almost-sure winning in stochastic two-player concurrent games
+
+New/novel algorithms for two-player adversarial deception games:
+* Deceptive sure winning in dynamic hypergames on graphs under action misperception [Link]()
+* Stealthy deceptive sure winning in static hypergames on graphs under labeling misperception [Link]()
+* Stealthy deceptive almost-sure winning in static hypergames on graphs under labeling misperception [Link]()
+* Mechanism (game graph) design for optimal stealthy deception in static hypergames on graphs under labeling misperception [Link]()
+* Behavioral subjectively rationalizability in dynamic hypergames on graphs under specification misperception [Link]()
+
+New/novel algorithms for preference-based planning in MDP:
+* Safe and positively improving strategies in MDP with incomplete preferences over temporal objectives [Link]()
+* Safe and almost-surely improving strategies in MDP with incomplete preferences over temporal objectives [Link]()
+* ✅ Non-dominated strategies in MDP with incomplete preferences over temporal objectives [Link]()
+
+New/novel algorithms for two/multi-player games with partial preferences:
+* Nash equilibrium in two-player games with adversarial preferences [Link]()
+* Privacy-aware Nash equilibrium in two-player games with  [Link]()
+* Admissibility in multi-player coalition games with incomplete preferences [Link]()
+
+
+
+
+# Installation Instructions
+
+## PRISM Games (Windows)
+
+Download and follow installation instructions from the following link:
+https://www.prismmodelchecker.org/games/download.php 
+
+Ensure JRE v9+ is available to run PRISM and PRISM-games.
+
+> Note: If the `load_prism` function may returns  `java.lang.ClassNotFoundException: prism.PrismCL` error, then 
+> modify the `\PATH\TO\PRISM-GAMES-INSTALLATION\bin\prism.bat` as suggested by https://stackoverflow.com/questions/70800624/error-could-not-find-or-load-main-class-prism-prismcl-caused-by-java-lang-clas
+> The modified `prism.bat` file is provided in the `external` directory, which can be copied to prism-games' bin folder. 
+
+
+# How to setup and solve a game? 
+
+1. Decide how to model the game: 
+    transition system: by defining `S, A, T, AP, L, ...` components
+    module: by defining transition system similar to PRISM modules, but implemented in Python
+    PRISM model: using a `.prism` file.
+2. From `templates/` folder, copy the template file to implement the abstract `TSys, Module, PRISMGame` classes.
+3. Implement the relevant abstract methods of chosen model class.
+4. Select a flattened representation -- `GraphGame, MatrixGame, BDDGame` and create an instance.
+5. Call the `build` method on the game instance to flatten the game into its representation. 
+    Otherwise, leave it as it is if you will compute a symbolic product game etc.
+6. Define objectives, using any logic or automaton representation user wants.
+7. In case of automata-theoretic approach, the product must be implemented by user, unless 
+    it is already available for the class of games considered.
+8. The solvers are implemented in folders of various class of game.
--- examples/mdp_pref/automatica2025/sim.py
+++ examples/mdp_pref/automatica2025/sim.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pathlib import Path
 
+import numpy as np
 import prefltlf2pdfa.viz
 import pygame
 import scipy.stats
@@ -39,8 +40,7 @@ class BeeRobotEnv(Simulator):
     def __init__(self, game, monitors=None, config=CONFIG, **kwargs):
         super().__init__(game, monitors, **kwargs)
         self.config = config
-        self.size = max(CONFIG["num_rows"], CONFIG["num_columns"])
-        self._obstacles = CONFIG.get("obstacles", [])
+        self._obstacles = config.get("obstacles", [])
         self._show_automaton = kwargs.get("show_automaton", True)
         self.metadata["render_fps"] = kwargs.get("render_fps", 0.5)
         self.images = {
@@ -51,11 +51,11 @@ class BeeRobotEnv(Simulator):
             "daisy": pygame.image.load(Path().absolute() / "assets" / "daisy.png"),
         }
 
-        if self._show_automaton:
-            self.window_size = (1536, 512)
-        else:
-            self.window_size = (512, 512)
-        self.cell_size = self.window_size[1] // self.size
+        # Decide size of window and grid cells
+        self._max_dim = None
+        self._window_size = None
+        self._cell_size = None
+        self._configure_pygame_window()
 
     def delta(self, state, action):
         next_states = self.game.delta(state, action)
@@ -74,7 +74,7 @@ class BeeRobotEnv(Simulator):
         if self.window is None:
             pygame.init()
             pygame.display.init()
-            self.window = pygame.display.set_mode(self.window_size)
+            self.window = pygame.display.set_mode(self._window_size)
             pygame.display.set_caption("Bee Robot Environment")
             self.clock = pygame.time.Clock()
 
@@ -127,21 +127,28 @@ class BeeRobotEnv(Simulator):
 
     def _render_frame(self):
         # Create a canvas to draw on
-        canvas = pygame.Surface(self.window_size)
+        canvas = pygame.Surface(self._window_size)
         canvas.fill((255, 255, 255))  # White background
 
+        # Determine grid origin in window
+        grid_width = self.config["num_columns"] * self._cell_size
+        grid_height = self.config["num_rows"] * self._cell_size
+
+        # Calculate offsets to center the grid in the leftmost 500x500 region
+        grid_origin_x = (500 - grid_width) // 2
+        grid_origin_y = (500 - grid_height) // 2
+
         # Draw grid
-        self._draw_bird_bounds(canvas, self.cell_size)
-        self._draw_grid(canvas, self.cell_size)
-        self._draw_obstacles(canvas, self.cell_size)
-        self._draw_flower("tulip", canvas, self.cell_size)
-        self._draw_flower("daisy", canvas, self.cell_size)
-        self._draw_flower("orchid", canvas, self.cell_size)
-        self._draw_bird(canvas, self.cell_size)
+        self._draw_bird_bounds(canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
+        self._draw_grid(canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
+        self._draw_obstacles(canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
+        self._draw_flower("tulip", canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
+        self._draw_flower("daisy", canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
+        self._draw_flower("orchid", canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
+        self._draw_bird(canvas, self._cell_size, grid_origin_in_window=(grid_origin_x, grid_origin_y))
 
         actions = self.game.actions(self.state)
-        print(actions)
-        self._draw_bee(canvas, self.cell_size, actions)
+        self._draw_bee(canvas, self._cell_size, actions, grid_origin_in_window=(grid_origin_x, grid_origin_y))
 
         # Draw automaton
         if self._show_automaton:
@@ -149,104 +156,182 @@ class BeeRobotEnv(Simulator):
 
         return canvas
 
-    def _draw_grid(self, canvas, cell_size):
-        grid_color = (0, 0, 0)  # black
-        for x in range(self.size + 1):
-            pygame.draw.line(canvas, grid_color, (x * cell_size, 0), (x * cell_size, self.window_size[1]), 1)
-            pygame.draw.line(canvas, grid_color, (0, x * cell_size), (self.window_size[1], x * cell_size), 1)
+    def _configure_pygame_window(self):
+        # Assert gridworld dimensions are smaller than (10, 10)
+        assert self.config["num_rows"] <= 10 and self.config["num_columns"] <= 10, \
+            "Gridworld dimensions must be smaller than or equal to 10x10."
 
-    def _draw_obstacles(self, canvas, cell_size):
+        # Determine the maximum dimension of the gridworld
+        max_dim = max(self.config["num_rows"], self.config["num_columns"])
+
+        # Calculate cell size to fit within a 500x500 area
+        self._cell_size = 500 // max_dim
+
+        # Set window size based on whether automaton display is enabled
+        if self._show_automaton:
+            self._window_size = (1500, 500)  # 1500x500 if automaton is shown
+        else:
+            self._window_size = (500, 500)  # Square window for gridworld only
+
+    def _draw_grid(self, canvas, cell_size, grid_origin_in_window=(0, 0)):
+        grid_color = (0, 0, 0)  # Black
+        offset_x, offset_y = grid_origin_in_window
+        grid_width = self.config["num_columns"] * self._cell_size
+        grid_height = self.config["num_rows"] * self._cell_size
+
+        # Draw vertical lines
+        for x in range(self.config["num_columns"] + 1):
+            pygame.draw.line(
+                canvas,
+                grid_color,
+                (offset_x + x * cell_size, offset_y),
+                (offset_x + x * cell_size, offset_y + grid_height),
+                1,
+            )
+
+        # Draw horizontal lines
+        for y in range(self.config["num_rows"] + 1):
+            pygame.draw.line(
+                canvas,
+                grid_color,
+                (offset_x, offset_y + y * cell_size),
+                (offset_x + grid_width, offset_y + y * cell_size),
+                1,
+            )
+
+        # for x in range(self._max_dim + 1):
+        #     pygame.draw.line(canvas, grid_color, (x * cell_size, 0), (x * cell_size, self._window_size[1]), 1)
+        #     pygame.draw.line(canvas, grid_color, (0, x * cell_size), (self._window_size[1], x * cell_size), 1)
+
+    def _draw_obstacles(self, canvas, cell_size, grid_origin_in_window=(0, 0)):
+        offset_x, offset_y = grid_origin_in_window
         for obs in self._obstacles:
-            x, y = obs
-            top_left = (x * cell_size, y * cell_size)
-            bottom_right = ((x + 1) * cell_size, (y + 1) * cell_size)
-            top_right = ((x + 1) * cell_size, y * cell_size)
-            bottom_left = (x * cell_size, (y + 1) * cell_size)
+            x, y = self._transform_coordinates(obs)
+            top_left = (x * cell_size + offset_x, y * cell_size + offset_y)
+            bottom_right = ((x + 1) * cell_size + offset_x, (y + 1) * cell_size + offset_y)
+            top_right = ((x + 1) * cell_size + offset_x, y * cell_size + offset_y)
+            bottom_left = (x * cell_size + offset_x, (y + 1) * cell_size + offset_y)
             pygame.draw.line(canvas, (0, 0, 0), top_left, bottom_right, 2)
             pygame.draw.line(canvas, (0, 0, 0), top_right, bottom_left, 2)
 
-    def _draw_bird_bounds(self, canvas, cell_size):
+    def _draw_bird_bounds(self, canvas, cell_size, grid_origin_in_window=(0, 0)):
+        offset_x, offset_y = grid_origin_in_window
         for x, y in self.config["bird_bounds"]:
-            rect = pygame.Rect(x * cell_size, y * cell_size, cell_size, cell_size)
+            x, y = self._transform_coordinates((x, y))
+            rect = pygame.Rect(x * cell_size + offset_x, y * cell_size + offset_y, cell_size, cell_size)
             pygame.draw.rect(canvas, (250, 232, 232), rect)
 
-    def _draw_flower(self, param, canvas, cell_size):
-        x, y = self.config[f"{param}_loc"]
+    def _draw_flower(self, param, canvas, cell_size, grid_origin_in_window=(0, 0)):
+        offset_x, offset_y = grid_origin_in_window
+        x, y = self._transform_coordinates(self.config[f"{param}_loc"])
         flower_image = pygame.transform.scale(self.images[param], (0.75 * cell_size, 0.75 * cell_size))
-        offset = (cell_size - flower_image.get_width()) // 2
-        canvas.blit(flower_image, (x * cell_size + offset, y * cell_size + offset))
+        offset_img = (cell_size - flower_image.get_width()) // 2
+        canvas.blit(flower_image, (x * cell_size + offset_img + offset_x, y * cell_size + offset_img + offset_y))
 
-    def _draw_bird(self, canvas, cell_size):
-        x, y = self.state.game_state.bird_x, self.state.game_state.bird_y
+    def _draw_bird(self, canvas, cell_size, grid_origin_in_window=(0, 0)):
+        offset_x, offset_y = grid_origin_in_window
+        x, y = self._transform_coordinates((self.state.game_state.bird_x, self.state.game_state.bird_y))
         bird_image = pygame.transform.scale(self.images["bird"], (0.75 * cell_size, 0.75 * cell_size))
-        offset = (cell_size - bird_image.get_width()) // 2
-        canvas.blit(bird_image, (x * cell_size + offset, y * cell_size + offset))
+        offset_img = (cell_size - bird_image.get_width()) // 2
+        canvas.blit(bird_image, (x * cell_size + offset_img + offset_x, y * cell_size + offset_img + offset_y))
 
-    def _draw_bee(self, canvas, cell_size, actions):
+    def _draw_bee(self, canvas, cell_size, actions, grid_origin_in_window=(0, 0)):
+        offset_x, offset_y = grid_origin_in_window
+
         # Show bee position
-        x, y = self.state.game_state.bee_x, self.state.game_state.bee_y
-        bee_image = pygame.transform.scale(self.images["bee"], (0.75 * cell_size, 0.75 * cell_size))
-        offset = (cell_size - bee_image.get_width()) // 2
-        canvas.blit(bee_image, (x * cell_size + offset, y * cell_size + offset))
+        x, y = self._transform_coordinates((self.state.game_state.bee_x, self.state.game_state.bee_y))
+        bee_image = pygame.transform.scale(self.images["bee"], (0.5 * cell_size, 0.5 * cell_size))
+        offset_img = (cell_size - bee_image.get_width()) // 2
+        canvas.blit(bee_image, (x * cell_size + offset_img + offset_x, y * cell_size + offset_img + offset_y))
 
-        # Show possible actions
-        # Define arrow size
-        arrow_size = int(0.25 * cell_size)
+        # Render battery counter
+        font = pygame.font.Font(None, 24)  # Default font with size 24
+        battery_text = font.render(f"{self.state.game_state.battery}", True, (255, 0, 0))  # Red text
+        text_x = x * cell_size + offset_img + offset_x + bee_image.get_width() - battery_text.get_width() // 2
+        text_y = y * cell_size + offset_img + offset_y - battery_text.get_height() // 2
+        canvas.blit(battery_text, (text_x, text_y))
 
-        # # Draw arrows for available actions
-        # # Render arrows for each available action
-        # arrow_length = cell_size // 4  # Adjust arrow length to fit within the circle
-        # for action in actions:
-        #     start_pos = (x * cell_size + cell_size // 2, y * cell_size + cell_size // 2)
-        #     if action == "N":
-        #         # end_pos = (start_pos[0], start_pos[1] - arrow_length)
-        #         start_pos = (x * cell_size + cell_size // 2, y * cell_size)  # Top border
-        #         end_pos = (start_pos[0], start_pos[1] - arrow_length)
-        #         arrow_tip = [
-        #             (end_pos[0] - 3, end_pos[1] + 6),
-        #             (end_pos[0] + 3, end_pos[1] + 6),
-        #             end_pos
-        #         ]
-        #     elif action == "S":
-        #         # end_pos = (start_pos[0], start_pos[1] + arrow_length)
-        #         start_pos = (x * cell_size + cell_size // 2, y * cell_size)  # Top border
-        #         end_pos = (start_pos[0], start_pos[1] - arrow_length)
-        #         arrow_tip = [
-        #             (end_pos[0] - 3, end_pos[1] - 6),
-        #             (end_pos[0] + 3, end_pos[1] - 6),
-        #             end_pos
-        #         ]
-        #     elif action == "E":
-        #         # end_pos = (start_pos[0] + arrow_length, start_pos[1])
-        #         start_pos = (x * cell_size + cell_size, y * cell_size + cell_size // 2)  # Right border
-        #         end_pos = (start_pos[0] + arrow_length, start_pos[1])
-        #         arrow_tip = [
-        #             (end_pos[0] - 6, end_pos[1] - 3),
-        #             (end_pos[0] - 6, end_pos[1] + 3),
-        #             end_pos
-        #         ]
-        #     elif action == "W":
-        #         # end_pos = (start_pos[0] - arrow_length, start_pos[1])
-        #         start_pos = (x * cell_size, y * cell_size + cell_size // 2)  # Left border
-        #         end_pos = (start_pos[0] - arrow_length, start_pos[1])
-        #         arrow_tip = [
-        #             (end_pos[0] + 6, end_pos[1] - 3),
-        #             (end_pos[0] + 6, end_pos[1] + 3),
-        #             end_pos
-        #         ]
-        #     else:
-        #         continue
-        #
-        # color = (0, 0, 255)  # Blue color for the arrow
-        # # Draw the arrow line
-        # print(start_pos, end_pos)
-        # pygame.draw.line(canvas, color, start_pos, end_pos, 2)
-        #
-        # # Draw the arrowhead
-        # pygame.draw.polygon(canvas, color, arrow_tip)
+        # Render arrows for each available action
+        for action in actions:
+            if action == "N":
+                start_offset = (cell_size // 2, cell_size // 4)
+                end_offset = (cell_size // 2, 0)
+                start_pos = (
+                    x * cell_size + start_offset[0] + offset_x,
+                    y * cell_size + start_offset[1] + offset_y
+                )  # Top border
+                end_pos = (
+                    x * cell_size + end_offset[0] + offset_x,
+                    y * cell_size + end_offset[1] + offset_y
+                )
+
+                # end_pos = (start_pos[0], start_pos[1] - arrow_length)
+                arrow_tip = [
+                    (end_pos[0] - 3, end_pos[1] + 6),
+                    (end_pos[0] + 3, end_pos[1] + 6),
+                    end_pos
+                ]
+            elif action == "S":
+                start_offset = (cell_size // 2, (3 * cell_size) // 4)
+                end_offset = (cell_size // 2, cell_size)
+                start_pos = (
+                    x * cell_size + start_offset[0] + offset_x,
+                    y * cell_size + start_offset[1] + offset_y
+                )  # Bottom border
+                end_pos = (
+                    x * cell_size + end_offset[0] + offset_x,
+                    y * cell_size + end_offset[1] + offset_y
+                )
+                arrow_tip = [
+                    (end_pos[0] - 3, end_pos[1] - 6),
+                    (end_pos[0] + 3, end_pos[1] - 6),
+                    end_pos
+                ]
+            elif action == "E":
+                start_offset = ((3 * cell_size) // 4, cell_size // 2)
+                end_offset = (cell_size, cell_size // 2)
+                start_pos = (
+                    x * cell_size + start_offset[0] + offset_x,
+                    y * cell_size + start_offset[1] + offset_y
+                )  # Bottom border
+                end_pos = (
+                    x * cell_size + end_offset[0] + offset_x,
+                    y * cell_size + end_offset[1] + offset_y
+                )
+                arrow_tip = [
+                    (end_pos[0] - 6, end_pos[1] - 3),
+                    (end_pos[0] - 6, end_pos[1] + 3),
+                    end_pos
+                ]
+            elif action == "W":
+                start_offset = (cell_size // 4, cell_size // 2)
+                end_offset = (0, cell_size // 2)
+                start_pos = (
+                    x * cell_size + start_offset[0] + offset_x,
+                    y * cell_size + start_offset[1] + offset_y
+                )  # Bottom border
+                end_pos = (
+                    x * cell_size + end_offset[0] + offset_x,
+                    y * cell_size + end_offset[1] + offset_y
+                )
+
+                arrow_tip = [
+                    (end_pos[0] + 6, end_pos[1] - 3),
+                    (end_pos[0] + 6, end_pos[1] + 3),
+                    end_pos
+                ]
+            else:
+                continue
+
+            color = (0, 0, 255)  # Blue color for the arrow
+            # Draw the arrow line
+            pygame.draw.line(canvas, color, start_pos, end_pos, 2)
+
+            # Draw the arrowhead
+            pygame.draw.polygon(canvas, color, arrow_tip)
 
     def _draw_automaton(self, canvas):
-        # Extract relevant information
+        # Extract current automaton state information
         paut = self.game.aut
         sa_state = self.state.aut_state
         pg_state = None
@@ -256,7 +341,9 @@ class BeeRobotEnv(Simulator):
                 break
 
         # Convert automaton to DOT
-        sa_dot, pg_dot = prefltlf2pdfa.viz.paut2dot(paut=paut)
+        sa_dot, pg_dot = prefltlf2pdfa.viz.paut2dot(paut=paut, show_pg_state=True)
+        sa_dot.graph_attr.update({"dpi": "300"})
+        pg_dot.graph_attr.update({"dpi": "300"})
 
         # Set all node colors to white
         for node in sa_dot.nodes():
@@ -282,26 +369,32 @@ class BeeRobotEnv(Simulator):
         automaton_sa = pygame.image.load(".tmp/automaton_sa.png")
         automaton_pg = pygame.image.load(".tmp/automaton_pg.png")
 
-        # Scale images
-        automaton_sa = pygame.transform.scale(
-            automaton_sa,
-            (min(480, automaton_sa.get_width()), min(480, automaton_sa.get_height()))
-        )
-        automaton_pg = pygame.transform.scale(
-            automaton_pg,
-            (min(480, automaton_pg.get_width()), min(480, automaton_pg.get_height()))
-        )
+        # Show semi-automaton
+        max_dim = max(500, automaton_sa.get_width(), automaton_sa.get_height())
+        if max_dim > 500:
+            aspect_scale = 500 / max_dim
+            automaton_sa = pygame.transform.smoothscale_by(
+                automaton_sa,
+                aspect_scale * 0.9
+            )
+        x = 502 + (500 - automaton_sa.get_width()) // 2
+        y = (500 - automaton_sa.get_height()) // 2
+        canvas.blit(automaton_sa, (x, y))  # Centered in the leftmost 500x500 region
 
-        # Update canvas
-        # x = 4 * (self.window_size[1]) // 2 - automaton_sa.get_width() // 2
-        # y = (self.window_size[1]) // 2 - automaton_sa.get_height() // 2
-        canvas.blit(automaton_sa, (self.window_size[1] + 32, 32))  # Top-right corner
-        # canvas.blit(automaton_sa, (x, y))  # Top-right corner
+        # Show preference graph
+        max_dim = max(500, automaton_pg.get_width(), automaton_pg.get_height())
+        if max_dim > 500:
+            aspect_scale = 500 / max_dim
+            automaton_pg = pygame.transform.smoothscale_by(
+                automaton_pg,
+                aspect_scale * 0.9
+            )
+        x = 1000 + (500 - automaton_pg.get_width()) // 2
+        y = (500 - automaton_pg.get_height()) // 2
+        canvas.blit(automaton_pg, (x, y))  # Centered in the leftmost 500x500 region
 
-        # x = 6 * (self.window_size[1]) // 2 - automaton_sa.get_width() // 2
-        # y = (self.window_size[1]) // 2 - automaton_sa.get_height() // 2
-        canvas.blit(automaton_pg, (self.window_size[1] * 2 + 32, 32))  # Bottom-right corner
-        # canvas.blit(automaton_pg, (x, y))  # Bottom-right corner
+    def _transform_coordinates(self, cell: List[int]):
+        return cell[0], self.config["num_rows"] - cell[1] - 1
 
 
 def not_terminated(history):
